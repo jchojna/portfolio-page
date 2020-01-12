b5c563c744785fc034958a4335ccaa6ce60b95e7
Jakub Chojna <jakub.chojna@gmail.com>
2020-01-12 16:18:24 +0100

    feat(mobile header): change menu link visibility on section change

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -149,12 +149,12 @@ const handleMenuItemClick = (activeIndex) => {
         const sectionId = sections[index].id;
         if (index !== activeIndex) {
           // ! turn on
-          //item.classList.remove('menu__item--visible');
-          item.classList.remove('menu__item--animated');
+          item.classList.remove('menu__item--visible');
           item.style.top = 0;
           items[index].width = menuLinks[index].clientWidth;
           menuLinks[index].classList.add(`menu__link--intro-${sectionId}`);
         }
+        item.classList.remove('menu__item--animated');
         menuLinks[index].style.width = '100%';
       });
       //. handle introBox and menu background                     .//
@@ -268,10 +268,14 @@ const handleMobileHeader = () => {
     if (action === 'activate') {
       menuUpperBackground.classList.remove(`pageHeader__background--${currentId}`);
       burgerButton.classList.remove(`burgerButton--${currentId}`);
+      introBox.classList.remove(`pageHeader__introBox--${currentId}`);
+      menuItems[index].classList.remove('menu__item--visible');
 
     } else if (action === 'deactivate') {
       menuUpperBackground.classList.add(`pageHeader__background--${currentId}`);
       burgerButton.classList.add(`burgerButton--${currentId}`);
+      introBox.classList.add(`pageHeader__introBox--${currentId}`);
+      menuItems[index].classList.add('menu__item--visible');
 
     }
   }
