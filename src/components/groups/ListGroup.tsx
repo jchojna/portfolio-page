cc25feff034e3e6fca0a02128c80a2310fc3a857
Jakub Chojna <jakub.chojna@gmail.com>
2023-11-21 19:24:56 +0100

    chore: add features content to projects.json

--- src/components/groups/ListGroup.tsx
+++ src/components/groups/ListGroup.tsx
@@ -5,6 +5,7 @@ import classes from './Group.module.scss';
 const ListGroup = ({ title, projectName, content }) => {
   return (
     <div className={clsx(classes.group, classes[projectName])}>
+      {/* <span class="mark mark--category mark--tasktimer"></span> */}
       <h3 className={classes.heading}>{title}</h3>
       <ul className={classes.paragraphs}>
         {content.map((paragraph) => (
