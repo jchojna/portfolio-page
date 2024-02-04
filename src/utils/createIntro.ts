785c9fe348617690cd15a046c9e38b3792c8e5d1
Jakub Chojna <jakub.chojna@gmail.com>
2024-02-04 15:52:49 +0100

    refactor: render characters in createIntro

--- src/utils/createIntro.ts
+++ src/utils/createIntro.ts
@@ -1,5 +1,17 @@
 type TimeoutId = NodeJS.Timeout | undefined;
 
+const introText = 'jakub chojna frontend projects';
+export const media = {
+  sm: 380,
+  md: 768,
+  lg: 1200,
+};
+const introItemWidth =
+  window.innerWidth >= media.lg ? 35 : window.innerWidth >= media.md ? 35 : 20;
+const introItemHeight = 2 * introItemWidth;
+// const visuals = document.querySelector('.visuals--js')!;
+
+// intro
 let introFirstTimeoutId: TimeoutId = undefined;
 let introSecondTimeoutId: TimeoutId = undefined;
 let introThirdTimeoutId: TimeoutId = undefined;
@@ -7,16 +19,81 @@ let introForthTimeoutId: TimeoutId = undefined;
 let introCharIntervalId: TimeoutId = undefined;
 const introFirstTimeoutInterval = 600;
 
-const createIntro = (
-  introText,
-  media,
-  loaderRef,
-  endingBeforeRef,
-  endingAfterRef,
-  gridRef,
-  skipButtonRef,
-  introItemWidth,
-  introItemHeight
+const setIntroLoaderPosition = (loader: HTMLElement) => {
+  loader.style.top = `${loader.offsetTop}px`;
+  loader.style.left = `${loader.offsetLeft}px`;
+};
+
+const createGridItems = (
+  classes,
+  container: HTMLElement,
+  introText: string,
+  introItemWidth: number,
+  introItemHeight: number
+) => {
+  const intro = [...introText]
+    .map((char) => {
+      return char !== ' '
+        ? `<li
+              class="${classes.gridItem}"
+              style="width: ${introItemWidth}px; height: ${introItemHeight}px;"
+            >
+              <div class="${classes.gridCharacter} gridCharacter">
+                <svg
+                  class="${classes.svgCharacter}"
+                  viewBox="0 0 50 100"
+                >
+                  <use href="assets/svg/letters.svg#${char}"></use>
+                </svg>
+                <svg
+                  class="${classes.svgShadow}"
+                  viewBox="0 0 50 100"
+                >
+                  <use href="assets/svg/letters.svg#${char}-shadow"></use>
+                </svg>
+              </div>
+            </li>`
+        : `<li
+              class="${classes.gridItem}"
+              style="width: ${introItemWidth}px; height: ${introItemHeight}px;"
+            >
+              <div class="${classes.gridSeparator} gridCharacter"></div>
+            </li>`;
+    })
+    .join('');
+  container.innerHTML = intro;
+};
+
+// assign size and position of one element to another
+const setSizeAndPosition = (
+  element: HTMLElement,
+  target: HTMLElement,
+  size?: number
+) => {
+  element.style.top = `${target.offsetTop}px`;
+  element.style.left = `${target.offsetLeft}px`;
+  element.style.width = size ? `${size}px` : `${target.clientWidth}px`;
+  element.style.height = size ? `${size}px` : `${target.clientHeight}px`;
+};
+
+const handleIntroLoader = (
+  loader: HTMLElement,
+  endingBefore: HTMLElement,
+  transitionClass: string
+) => {
+  loader.classList.add(transitionClass);
+  loader.style.transitionDuration = `${introFirstTimeoutInterval}ms`;
+  setSizeAndPosition(loader, endingBefore, introItemHeight);
+};
+
+const handleIntroAnimation = (
+  classes,
+  intro,
+  loader,
+  grid,
+  endingBefore,
+  endingAfter,
+  skipButton
 ) => {
   let charIndex = 0;
   const charTotal = introText.length;
@@ -31,8 +108,8 @@ const createIntro = (
   let gridTopMargin: number | null = null;
 
   // intervals
-  const loadCharInterval = 25; //30
-  const translateCharInterval = 0; //100
+  const loadCharInterval = 25;
+  const translateCharInterval = 0;
   const introSecondTimeoutInterval = loadCharInterval * charTotal + 1000;
   const introGridViewInterval = 2000;
   const inBetweenTransition = 500;
@@ -45,8 +122,8 @@ const createIntro = (
     let endingAfterLeft: number | undefined = undefined;
 
     if (index < charTotal) {
-      const beforeChild = gridRef.current.children[0];
-      const afterChild = gridRef.current.children[index];
+      const beforeChild = grid.children[0];
+      const afterChild = grid.children[index];
       if (beforeChild instanceof HTMLElement) {
         endingBeforeTop = beforeChild.offsetTop;
         endingBeforeLeft = beforeChild.offsetLeft - introItemWidth;
@@ -56,7 +133,7 @@ const createIntro = (
         endingAfterLeft = afterChild.offsetLeft;
       }
     } else {
-      const prevAfterChild = gridRef.current.children[index - 1];
+      const prevAfterChild = grid.children[index - 1];
       let prevAfterChildOffset: number = 0;
       if (prevAfterChild instanceof HTMLElement) {
         prevAfterChildOffset = prevAfterChild.offsetLeft;
@@ -64,48 +141,21 @@ const createIntro = (
       endingAfterLeft = prevAfterChildOffset + introItemWidth;
     }
 
-    endingBeforeRef.current.style.top = `${endingBeforeTop}px`;
-    endingBeforeRef.current.style.left = `${endingBeforeLeft}px`;
-    endingAfterRef.current.style.top = `${endingAfterTop}px`;
-    endingAfterRef.current.style.left = `${endingAfterLeft}px`;
-  };
-
-  const setIntroLoaderPosition = () => {
-    if (!loaderRef.current) return;
-    loaderRef.current.style.top = `${loaderRef.current.offsetTop}px`;
-    loaderRef.current.style.left = `${loaderRef.current.offsetLeft}px`;
-  };
-
-  // assign size and position of one element to another
-  const setSizeAndPosition = (
-    element: HTMLElement,
-    target: HTMLElement,
-    size?: number
-  ) => {
-    element.style.top = `${target.offsetTop}px`;
-    element.style.left = `${target.offsetLeft}px`;
-    element.style.width = size ? `${size}px` : `${target.clientWidth}px`;
-    element.style.height = size ? `${size}px` : `${target.clientHeight}px`;
-  };
-
-  const handleIntroLoader = () => {
-    loaderRef.current.style.transitionDuration = `${introFirstTimeoutInterval}ms`;
-    setSizeAndPosition(
-      loaderRef.current,
-      endingBeforeRef.current,
-      introItemHeight
-    );
+    endingBefore.style.top = `${endingBeforeTop}px`;
+    endingBefore.style.left = `${endingBeforeLeft}px`;
+    endingAfter.style.top = `${endingAfterTop}px`;
+    endingAfter.style.left = `${endingAfterLeft}px`;
   };
 
   // show consecutive characters of intro text
   const loadChar = () => {
     if (charIndex < charTotal) {
-      const currentItem = gridRef.current.children[charIndex];
-      currentItem.style.visibility = 'inherit';
+      const currentItem = grid.children[charIndex];
       if (currentItem instanceof HTMLElement) {
         currentItem.style.width = `${introItemWidth}px`;
         currentItem.style.height = `${introItemHeight}px`;
       }
+      currentItem.classList.add(classes.visible);
       setEndings(charIndex);
       charIndex++;
     } else {
@@ -114,20 +164,16 @@ const createIntro = (
     }
   };
 
-  // animate characters position on introGrid change
+  // animate characters position on grid change
   const handleChars = (chars: NodeListOf<HTMLElement>, isInitial: boolean) => {
     if (isInitial) {
       [...chars].forEach((char, index) => {
-        const { offsetTop, offsetLeft } = gridRef.current.children[
-          index
-        ] as HTMLElement;
+        const { offsetTop, offsetLeft } = grid.children[index];
         char.style.top = `${offsetTop}px`;
         char.style.left = `${offsetLeft}px`;
         char.style.width = `${introItemWidth}px`;
         char.style.height = `${introItemHeight}px`;
-        char.style.position = 'fixed';
-
-        // char.classList.add('grid__char--transition');
+        char.classList.add(classes.transition);
       });
     } else {
       [...chars].forEach((char, index) => {
@@ -137,71 +183,58 @@ const createIntro = (
             : 0;
 
         if (index >= maxColNum - bias) {
-          const { offsetTop, offsetLeft } = gridRef.current.children[
-            index
-          ] as HTMLElement;
+          const { offsetTop, offsetLeft } = grid.children[index];
           char.style.top = `${offsetTop}px`;
           char.style.left = `${offsetLeft}px`;
-          char.style.fill = 'inherit';
+          char.classList.add(classes.color);
         }
       });
     }
   };
 
-  // calculate introGrid's gaps and items paddings
+  // calculate grid's gaps and items paddings
   const getColGap = () => {
     const rowNum = charTotal / minColNum;
     const gridHeight = rowNum * introItemHeight + (rowNum - 1) * rowGap;
     return (gridHeight - minColNum * introItemWidth) / (minColNum - 1);
   };
 
-  // set introGrid's top margin
+  // set grid's top margin
   const updateTopMargin = () => {
-    const topMargin = (window.innerHeight - gridRef.current.clientHeight) / 2;
+    const topMargin = (window.innerHeight - grid.clientHeight) / 2;
 
     if (topMargin !== gridTopMargin || gridTopMargin === null) {
       gridTopMargin = topMargin;
-      gridRef.current.style.marginTop = `${gridTopMargin}px`;
+      grid.style.marginTop = `${gridTopMargin}px`;
     }
   };
 
-  setIntroLoaderPosition();
-  handleIntroLoader();
-  // configure introGrid on start
-  gridRef.current.style.display = 'grid';
-  gridRef.current.style.gridTemplateColumns = `repeat(${maxColNum}, 1fr)`;
+  // configure grid on start
+  grid.classList.add(classes.visible);
+  grid.style.gridTemplateColumns = `repeat(${maxColNum}, 1fr)`;
   updateTopMargin();
 
   // set sizes and position of ending elements
-  endingBeforeRef.current.style.width = `${introItemWidth}px`;
-  endingBeforeRef.current.style.height = `${introItemHeight}px`;
-  endingAfterRef.current.style.width = `${introItemWidth}px`;
-  endingAfterRef.current.style.height = `${introItemHeight}px`;
+  endingBefore.style.width = `${introItemWidth}px`;
+  endingBefore.style.height = `${introItemHeight}px`;
+  endingAfter.style.width = `${introItemWidth}px`;
+  endingAfter.style.height = `${introItemHeight}px`;
   setEndings(charIndex);
 
-  // FIRST TIMEOUT
   clearTimeout(introFirstTimeoutId);
   clearTimeout(introSecondTimeoutId);
   clearTimeout(introThirdTimeoutId);
   clearTimeout(introForthTimeoutId);
 
+  // FIRST TIMEOUT
   introFirstTimeoutId = setTimeout(() => {
-    skipButtonRef.current.style.opacity = '1';
-    skipButtonRef.current.style.visibility = 'visible';
-    skipButtonRef.current.style.transition =
-      'color 0.2s, opacity 0.3s, visibility 0s';
+    skipButton.classList.add(classes.visible);
 
     // show ending elements
-    endingBeforeRef.current.style.opacity = 1;
-    endingBeforeRef.current.style.visibility = 'inherit';
-    endingBeforeRef.current.style.transition = 'opacity 0s, visibility 0s';
-    endingAfterRef.current.style.opacity = 1;
-    endingAfterRef.current.style.visibility = 'inherit';
-    endingAfterRef.current.style.transition = 'opacity 0s, visibility 0s';
-
-    loaderRef.current.style.opacity = 0;
-    loaderRef.current.style.visibility = 'hidden';
-    loaderRef.current.style.transition = 'opacity 0.5s, visibility 0s';
+    endingBefore.classList.add(classes.visible);
+    endingAfter.classList.add(classes.visible);
+    loader.classList.add(classes.hidden);
+    loader.classList.remove(classes.transition);
 
     // remove temporary child
     introCharIntervalId = setInterval(() => {
@@ -212,53 +245,43 @@ const createIntro = (
     // SECOND TIMEOUT
     introSecondTimeoutId = setTimeout(() => {
       // assign fixed positioning to svg elements
-      const gridChars = [...gridRef.current.children].map(
-        (child) => child.firstElementChild
-      );
-
-      endingBeforeRef.current.style.opacity = null;
-      endingBeforeRef.current.style.visibility = null;
-      endingBeforeRef.current.style.transition = null;
-      endingAfterRef.current.style.opacity = null;
-      endingAfterRef.current.style.visibility = null;
-      endingAfterRef.current.style.transition = null;
+      const gridChars = grid.querySelectorAll('.gridCharacter');
+      endingAfter.classList.remove(classes.visible);
+      endingBefore.classList.remove(classes.visible);
       handleChars(gridChars, true);
-      // return;
 
-      // set introGrid's column and row gaps to make introGrid a square
+      // set grid's column and row gaps to make grid a square
       const columnGap = getColGap();
-      gridRef.current.style.columnGap = `${columnGap}px`;
-      gridRef.current.style.rowGap = `${rowGap}px`;
+      grid.style.columnGap = `${columnGap}px`;
+      grid.style.rowGap = `${rowGap}px`;
 
       // decrease number of grid columns
       introCharIntervalId = setInterval(() => {
         if (maxColNum >= minColNum) {
-          gridRef.current.style.gridTemplateColumns = `repeat(${maxColNum--}, 1fr)`;
+          grid.style.gridTemplateColumns = `repeat(${maxColNum--}, 1fr)`;
           handleChars(gridChars, false);
           updateTopMargin();
-          // debugger;
         } else {
           // when interval ends
           clearInterval(introCharIntervalId);
-          setSizeAndPosition(loaderRef.current, gridRef.current);
+          setSizeAndPosition(loader, grid);
 
           // show intro loader
-          loaderRef.current.style.opacity = 1;
-          loaderRef.current.style.visibility = 'inherit';
+          loader.classList.remove(classes.hidden);
           const delay = introGridViewInterval - inBetweenTransition;
-          loaderRef.current.style.transition = `
+          loader.style.transition = `
             opacity ${inBetweenTransition}ms ${delay}ms,
             visibility 0s ${delay}ms
           `;
 
           // THIRD TIMEOUT
           introThirdTimeoutId = setTimeout(() => {
-            skipButtonRef.current.style = null;
-
-            loaderRef.current.style.transition = null;
-            loaderRef.current.style.transitionDuration = `${inBetweenTransition}ms`;
-            gridRef.current.classList.remove('visible');
-            // setSizeAndPosition(loaderRef.current, introBox);
+            skipButton.classList.remove(classes.visible);
+            loader.classList.add(classes.transition);
+            loader.style.transition = '';
+            loader.style.transitionDuration = `${inBetweenTransition}ms`;
+            grid.classList.remove(classes.visible);
+            // setSizeAndPosition(loader, introBox);
 
             // FORTH TIMEOUT
             introForthTimeoutId = setTimeout(() => {
@@ -269,7 +292,7 @@ const createIntro = (
               // show introBox
               // visuals.classList.add('visuals--visible');
               // hide intro
-              // setIntroVisible(false);
+              // intro.classList.add('intro--hidden');
               // show page header
               // pageHeader.classList.add('pageHeader--visible');
             }, inBetweenTransition);
@@ -278,6 +301,29 @@ const createIntro = (
       }, translateCharInterval);
     }, introSecondTimeoutInterval);
   }, introFirstTimeoutInterval);
+};
+
+export const createIntro = (
+  classes,
+  intro: HTMLElement,
+  loader: HTMLElement,
+  grid: HTMLElement,
+  endingBefore: HTMLElement,
+  endingAfter: HTMLElement,
+  skipButton: HTMLElement
+) => {
+  createGridItems(classes, grid, introText, introItemWidth, introItemHeight);
+  setIntroLoaderPosition(loader);
+  handleIntroLoader(loader, endingBefore, classes.transition);
+  handleIntroAnimation(
+    classes,
+    intro,
+    loader,
+    grid,
+    endingBefore,
+    endingAfter,
+    skipButton
+  );
 };
 
 export default createIntro;
--- src/views/Intro.tsx
+++ src/views/Intro.tsx
@@ -5,19 +5,10 @@ import createIntro from '../utils/createIntro';
 
 import classes from './Intro.module.scss';
 
-const media = {
-  sm: 380,
-  md: 768,
-  lg: 1200,
-};
-const introText = 'jakub chojna frontend projects';
-const introItemWidth =
-  window.innerWidth >= media.lg ? 35 : window.innerWidth >= media.md ? 35 : 20;
-const introItemHeight = 2 * introItemWidth;
-
 const Intro = () => {
   const [isIntroVisible, setIntroVisible] = useState<boolean>(true);
 
+  const introRef = useRef<HTMLDivElement>(null);
   const loaderRef = useRef<HTMLDivElement>(null);
   const endingBeforeRef = useRef<HTMLDivElement>(null);
   const endingAfterRef = useRef<HTMLDivElement>(null);
@@ -26,20 +17,21 @@ const Intro = () => {
 
   useEffect(() => {
     createIntro(
-      introText,
-      media,
-      loaderRef,
-      endingBeforeRef,
-      endingAfterRef,
-      gridRef,
-      skipButtonRef,
-      introItemWidth,
-      introItemHeight
+      classes,
+      introRef.current,
+      loaderRef.current,
+      gridRef.current,
+      endingBeforeRef.current,
+      endingAfterRef.current,
+      skipButtonRef.current
     );
   }, []);
 
   return (
-    <div className={clsx(classes.intro, isIntroVisible && classes.visible)}>
+    <div
+      ref={introRef}
+      className={clsx(classes.intro, isIntroVisible && classes.visible)}
+    >
       <div ref={loaderRef} className={classes.loader}></div>
       <div
         ref={endingBeforeRef}
@@ -49,43 +41,7 @@ const Intro = () => {
         ref={endingAfterRef}
         className={clsx(classes.ending, classes.after)}
       ></div>
-      <ul ref={gridRef} className={classes.grid}>
-        {[...introText].map((char, index) => {
-          return char !== ' ' ? (
-            <li
-              key={index}
-              className={classes.gridItem}
-              style={{
-                width: `${introItemWidth}px`,
-                height: `${introItemHeight}px`,
-              }}
-            >
-              <div className={classes.char}>
-                <svg className={classes.svg} viewBox="0 0 50 100">
-                  <use href={`assets/svg/letters.svg#${char}`}></use>
-                </svg>
-                <svg
-                  className={clsx(classes.svg, classes.shadow)}
-                  viewBox="0 0 50 100"
-                >
-                  <use href={`assets/svg/letters.svg#${char}-shadow`}></use>
-                </svg>
-              </div>
-            </li>
-          ) : (
-            <li
-              key={index}
-              className={classes.gridItem}
-              style={{
-                width: `${introItemWidth}px`,
-                height: `${introItemHeight}px`,
-              }}
-            >
-              <div className={clsx(classes.char, classes.separator)}></div>
-            </li>
-          );
-        })}
-      </ul>
+      <ul ref={gridRef} className={classes.grid}></ul>
       <button ref={skipButtonRef} className={classes.skipButton}>
         Skip Intro
       </button>
