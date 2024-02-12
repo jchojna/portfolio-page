ec8fcdad309e322118bb48acb61f9628315f5ca3
Jakub Chojna <jakub.chojna@gmail.com>
2024-02-12 21:31:17 +0100

    feat: add carousel component

--- src/views/Project.tsx
+++ src/views/Project.tsx
@@ -1,5 +1,9 @@
 import clsx from 'clsx';
+import Carousel from 'nuka-carousel';
 import { useContext } from 'react';
+import img1 from '../assets/img/img1.png';
+import img2 from '../assets/img/img2.png';
+import img3 from '../assets/img/img3.png';
 
 import Graphic from '../components/Graphic';
 import ProjectFeatures from '../components/ProjectFeatures';
@@ -41,12 +45,24 @@ const Project = ({
         )}
       >
         <TextGroup title="About Project" projectName={name} content={about} />
-        <ProjectFeatures
-          projectName={name}
-          title="Features"
-          content={features}
-        />
+        <ProjectFeatures projectName={name} content={features} />
         <IconsList view={name} icons={icons} />
+        <div className={classes.carousel}>
+          <Carousel
+            defaultControlsConfig={{
+              prevButtonText: (
+                <div className={clsx(classes.arrow, classes.left)}></div>
+              ),
+              nextButtonText: (
+                <div className={clsx(classes.arrow, classes.right)}></div>
+              ),
+            }}
+          >
+            <img src={img1} />
+            <img src={img2} />
+            <img src={img3} />
+          </Carousel>
+        </div>
         <ProjectLinks
           projectName={name}
           repoUrl={html_url}
