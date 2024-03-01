ccd15a0c6a7bb0c50dd21f40025089145d774d13
Jakub Chojna <jakub.chojna@gmail.com>
2024-03-01 20:30:23 +0100

    feat: add archviz snapshots

--- src/views/Projects.tsx
+++ src/views/Projects.tsx
@@ -1,14 +1,33 @@
 import { useQuery } from '@tanstack/react-query';
+// tasktimer snapshots
 import tasktimerSnap1 from '../assets/img/tasktimer/01.jpg';
 import tasktimerSnap2 from '../assets/img/tasktimer/02.jpg';
 import tasktimerSnap3 from '../assets/img/tasktimer/03.jpg';
 import tasktimerSnap4 from '../assets/img/tasktimer/04.jpg';
+// archviz snapshots
+import archvizSnap1 from '../assets/img/archviz/01.jpg';
+import archvizSnap2 from '../assets/img/archviz/02.jpg';
+import archvizSnap3 from '../assets/img/archviz/03.jpg';
+import archvizSnap4 from '../assets/img/archviz/04.jpg';
+import archvizSnap5 from '../assets/img/archviz/05.jpg';
+import archvizSnap6 from '../assets/img/archviz/06.jpg';
 
 import projects from '../content/projects.json';
 import Project from './Project';
 
-const snapshots = {
+const snapshots: { [key: string]: string[] } = {
   tasktimer: [tasktimerSnap1, tasktimerSnap2, tasktimerSnap3, tasktimerSnap4],
+  hydrapp: [''],
+  portfolio: [''],
+  archviz: [
+    archvizSnap1,
+    archvizSnap2,
+    archvizSnap3,
+    archvizSnap4,
+    archvizSnap5,
+    archvizSnap6,
+  ],
+  quotes: [''],
 };
 
 const Projects = () => {
@@ -32,7 +51,7 @@ const Projects = () => {
           features={features}
           icons={icons}
           fetchedData={data.find((repo: RepoObj) => repo.name === repoName)}
-          snapshots={snapshots['tasktimer']}
+          snapshots={snapshots[name]}
         />
       ))}
     </>
