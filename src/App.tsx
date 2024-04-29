6f20d1763e67acf0f07a175b0c4a4d48baf96ff8
Jakub Chojna <jakub.chojna@gmail.com>
2024-04-29 11:40:55 +0200

    chore: add lazy loading and suspense to projects

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import clsx from 'clsx';
-import { useEffect, useRef, useState } from 'react';
+import { Suspense, lazy, useEffect, useRef, useState } from 'react';
 import ReactGA from 'react-ga4';
 import { useMediaQuery } from 'react-responsive';
 
+import classes from './App.module.scss';
 import Header from './components/Header';
+import MobileHeader from './components/MobileHeader';
 import About from './views/About';
 import Contact from './views/Contact';
 import CurrentViewContext from './views/CurrentViewContext';
 import Intro from './views/Intro';
-import Projects from './views/Projects';
 import Resume from './views/Resume';
 
-import classes from './App.module.scss';
-import MobileHeader from './components/MobileHeader';
+const Projects = lazy(() => import('./views/Projects'));
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -72,7 +72,9 @@ function App() {
           <div ref={sectionsRef} className={sectionsClass}>
             <About />
             <Resume />
-            {isIntroDone && <Projects />}
+            <Suspense fallback={<div>Loading Projects...</div>}>
+              {isIntroDone && <Projects />}
+            </Suspense>
             <Contact />
           </div>
         </div>
